4e92b337372316798d7dde49a113bf290ebc3189
Mark Ian Braun <icmagmilch@gmail.con>
2023-11-03 22:28:07 +0100

    added working connection to db

--- src/app.py
+++ src/app.py
@@ -1,21 +1,40 @@
 from flask import Flask, render_template, request
 import sqlite3
 import os
+from user import User
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(current_dir, '..', 'data', 'crypto_app.db')
 
 app = Flask(__name__)
+# create User object
+user = User()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    print('POST')
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        return render_template('login.html', username=username, password=password)
-    else:
-        return render_template('login.html')
+        user.username = request.form['username']
+        user.password = request.form['password']
+        if request.form['submit_button'] == 'login':
+            query = f'SELECT * FROM user WHERE username="{user.username}" AND password="{user.password}"'
+            conn = sqlite3.connect(db_path)
+            c = conn.cursor()
+            c.execute(query)
+            result = c.fetchone()
+            if result:
+                return render_template('home.html', username=user.username)
+            else:
+                return render_template('login.html', error='Invalid username or password')
+        if request.form['submit_button'] == 'register':
+            query = f'INSERT INTO user (username, password) VALUES ("{user.username}", "{user.password}")'
+            conn = sqlite3.connect(db_path)
+            c = conn.cursor()
+            c.execute(query)
+            conn.commit()
+            return render_template('login.html', msg='registerd successfully')
+
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
--- src/user.py
+++ src/user.py
@@ -1,17 +1,29 @@
 class User:
-    def __init__(self, username, password, user_id=None):
-        self.user_id = user_id
-        self.username = username
-        self.password = password
+    def __init__(self, username=None, password=None, user_id=None):
+        self._user_id = user_id
+        self._username = username
+        self._password = password
 
     @property
     def user_id(self):
-        return self.user_id
+        return self._user_id
 
     @property
     def username(self):
-        return self.username
+        return self._username
 
     @property
     def password(self):
-        return self.password
+        return self._password
+
+    @user_id.setter
+    def user_id(self, user_id):
+        self._user_id = user_id
+
+    @username.setter
+    def username(self, username):
+        self._username = username
+
+    @password.setter
+    def password(self, password):
+        self._password = password
