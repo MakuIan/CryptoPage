3466e1a91f90ab729995321f522c76dce975f9c5
Mark Ian Braun <icmagmilch@gmail.con>
2023-11-06 17:22:26 +0100

    finished search without add

--- src/home.py
+++ src/home.py
@@ -8,20 +8,51 @@ import requests  # pylint: disable=import-error
 from apikey import CRYPTO_COMPARE_API_KEY as CRYPTO_COMPARE  # pylint: disable=import-error
 
 
+def get_crypto_data(symbol):
+    # Set up the API request parameters
+    try:
+        params = {
+            'asset_symbol': symbol.upper(),
+            'api_key': CRYPTO_COMPARE,
+        }
+
+        # Send the API request and parse the response
+        response = requests.get(
+            'https://data-api.cryptocompare.com/asset/v1/data/by/symbol', params=params)
+        data = response.json()
+
+        # Extract the relevant data from the response
+        symbol = data['Data']['SYMBOL']
+        name = data['Data']['NAME']
+        logo = data['Data']['LOGO_URL']
+        price = data['Data']['PRICE_USD']
+
+        # Return the data as a dictionary
+        return {
+            'symbol': symbol,
+            'name': name,
+            'logo': logo,
+            'price': price,
+        }
+    except KeyError:
+        return None
+
+
 def home():
     """
     Render home page.
     """
-    # if request.form['home_btn'] == 'search_btn':
-    #     params = {
-    #         'asset_symbol': 'BTC',
-    #         'api_key': CRYPTO_COMPARE,
-    #     }
+    if request.form['home_btn'] == 'search_btn':
+        search = request.form['search-input']
+        res = get_crypto_data(search)
+        if res is None:
+            return render_template('home.html', id=session['id'], username=session['username'], error='Invalid Symbol')
+        data = {
+            'symbol': res['symbol'],
+            'name': res['name'],
+            'logo': res['logo'],
+            'price': res['price'],
+        }
 
-    #     response = requests.get(
-    #         'https://data-api.cryptocompare.com/asset/v1/data/by/symbol?', params=params)
-    #     data = response.json()
-    #     symbol = data['Data']['SYMBOL']
-    #     name = data['Data']['NAME']
-    #     return render_template('home.html', id=session['id'], username=session['username'], symbol=symbol, name=name, )
-    return render_template('home.html', id=session['id'], username=session['username'], post='true')
+        return render_template('home.html', id=session['id'], username=session['username'], data=data)
+    return render_template('home.html', id=session['id'], username=session['username'])
---
+++ src/static/displayData.js
@@ -0,0 +1,3 @@
+search.classList.add("open-popup");
+const element = document.querySelector(".search-container");
+element.style.marginTop = "160px";
